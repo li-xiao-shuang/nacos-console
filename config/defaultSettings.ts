f7e3f3df77a0df848731e413b954165bdab177d7
lixiaoshuang <644968328@qq.com>
2022-03-27 00:09:13 +0800

    修改国际化

---
+++ config/defaultSettings.ts
@@ -0,0 +1,21 @@
+import { Settings as LayoutSettings } from '@ant-design/pro-layout';
+
+const Settings: LayoutSettings & {
+  pwa?: boolean;
+  logo?: string;
+} = {
+  navTheme: 'light',
+  primaryColor: '#1890ff',
+  layout: 'side',
+  contentWidth: 'Fluid',
+  fixedHeader: false,
+  fixSiderbar: true,
+  colorWeak: false,
+  title: '',
+  pwa: false,
+  footerRender: false,
+  logo: 'https://nacos.io/img/nacos_colorful.png',
+  iconfontUrl: '',
+};
+
+export default Settings;
